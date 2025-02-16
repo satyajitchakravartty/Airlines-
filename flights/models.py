ffe52c6d4f5205c1ff9f7f0eca7cf6cbc9bd7bf5
Satyajit Chakravartty <shanuchakravartty@gmail.com>
2025-02-17 02:29:36 +0530

    Added Models and SQL

--- flights/models.py
+++ flights/models.py
@@ -1,4 +1,12 @@
+from django.db import models
+
 # Create your models here.
 
 # Model is a table which stores information
 
+class Flights(models.Model):
+    origin = models.CharField(max_length=64)
+    destination = models.CharField(max_length=64)
+    duration = models.IntegerField()
+
+    
