9e01ea141e2413d8c6508098010d82ff3849a511
Satyajit Chakravartty <shanuchakravartty@gmail.com>
2025-02-22 17:12:36 +0530

    Added 2nd flight, created super user Shawn, used admin interface to add more airports and flight

--- flights/admin.py
+++ flights/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+# Import Flight and Airport from models
+from .models import Flight, Airport
+
 # Register your models here.
+
+
+# This tell the django admin app, that i will want to use the admin app to be able to manipulate Airport and FLights
+admin.site.register(Airport)
+admin.site.register(Flight)
--- flights/models.py
+++ flights/models.py
@@ -12,11 +12,14 @@ class Airport(models.Model):
         return f"{self.city} ({self.code})"
 
 class Flight(models.Model):
-    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures") # We use cascade, in case we delete an airport from airport database, its going to also delete any corresponding flights.
-    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals") # we use related names to allow django to form a reverse relationship such as all flights expected at a particular destination.
+    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures") # We use cascade, 
+    # in case we delete an airport from airport database, its going to also delete any corresponding flights.
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals") 
+    # we use related names to allow django to form a reverse relationship such as all flights expected at a particular destination.
     duration = models.IntegerField()
 
     def __str__(self): # Every model can implement str function, which returns a string representaion of that particular object.
-        return f"{self.id}: {self.origin} to {self.destination}" # String representation of any flight is going to be a string that gives it an id which is equal to orgin to dest.
+        return f"{self.id}: {self.origin} to {self.destination}" 
+        # String representation of any flight is going to be a string that gives it an id which is equal to orgin to dest.
         
 
