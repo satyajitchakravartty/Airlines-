cefb1146dd62e4727af842f4bc530f722e982756
Satyajit Chakravartty <shanuchakravartty@gmail.com>
2025-02-23 03:32:03 +0530

    Added Authentication pt 2 - Adding authentication logic by updating views.py to check if user is correct

--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+# For user authentication
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
@@ -12,6 +14,27 @@ def index(request):
         return HttpResponseRedirect(reverse("login"))
     
 def login_view(request):
+    # Add logic if the request method is POST
+    if request.method == "POST":
+        # get the username and password
+        username = request.POST["username"]
+        password = request.POST["password"]
+        # Authenticate the user - if the request, username and pass is valid, give me back the actual user
+        user = authenticate(request, username=username, password=password)
+        # As long as user is not none ie user exists
+        if user is not None:
+            # The authentication is successful, and log the user in using request and user details
+            login(request, user)
+            # redirect them to the index route
+            return HttpResponseRedirect(reverse("index"))
+        # If user does not exist  
+        else:
+            # Return to the login page with the context message saying 'Invalid credentials'
+            return render(request, "users/login.html", {
+                "message": "Invalid credentials"
+            })
+
+
     return render(request, "users/login.html")
 
 def logout_view(request):
