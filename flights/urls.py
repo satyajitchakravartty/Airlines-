11a797c7771d6c32e2ae9b5fb96ae020979fe5be
Satyajit Chakravartty <shanuchakravartty@gmail.com>
2025-02-23 02:38:52 +0530

    Added feature which gives visitors to our site the ability to book a flight, submit button logic not set up set

--- flights/urls.py
+++ flights/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<int:flight_id>", views.flight, name="flight") # add a flight page to have individual pages for each flight id
+    path("<int:flight_id>", views.flight, name="flight"), # add a flight page to have individual pages for each flight id
+    path("<int:flight_id>/book", views.book, name="book") # Add a book page to book passengers and add them to a flight
 ]
--- flights/views.py
+++ flights/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Flight
+from .models import Flight, Passenger
 
 # Create your views here.
 def index(request):
@@ -12,5 +14,21 @@ def flight(request, flight_id): # Create a flight function that accepts flight_i
     flight = Flight.objects.get(pk=flight_id) # Get the flight whos ID = Flight ID ( pk refers to primary key, can also use id instead of pk)
     return render(request, "flights/flight.html", {
         "flight": flight, # pass the flight class to flight.html
-        "passengers": flight.passengers.all()
+        "passengers": flight.passengers.all(),
+        # list of passengers that are currently on the flight are excluded, rmeaining passenger list is captured in non_passengers.
+        "non_passengers": Passenger.objects.exclude(flights=flight).all() 
     })
+
+def book(request, flight_id):
+    # If the user is trying to submit the form
+    if request.method == "POST":  
+        # get the flight id details and store it in flight
+        flight = Flight.objects.get(pk=flight_id)
+        # Now if someone books a new passenger on this flight, 
+        # they should mention the id of the passenger to know which passenger to book on the flight
+        passenger = Passenger.object.get(pk=int(request.POST["passenger"])) # User sends the passenger ID via the form with an input field called "passenger". Convert it into an int.
+        # Using the above info, do the following:
+        # logic to create a new row into the table keeping track of passengers in that flight
+        passenger.flights.add(flight)
+        # Now redirect them to the flight route
+        return HttpResponseRedirect(reverse("flight", args=(flight.id,)))
