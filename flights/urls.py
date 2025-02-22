6c877cb18c1a0e01c0954f3402d5a1d709a78f41
Satyajit Chakravartty <shanuchakravartty@gmail.com>
2025-02-22 18:10:09 +0530

    added individual  flight pages for each flight based on  flight id

--- flights/urls.py
+++ flights/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("<int:flight_id>", views.flight, name="flight") # add a flight page to have individual pages for each flight id
 ]
--- flights/views.py
+++ flights/views.py
@@ -7,3 +7,9 @@ def index(request):
     return render(request, "flights/index.html", {
         "flights": Flight.objects.all()
     })
+
+def flight(request, flight_id): # Create a flight function that accepts flight_id as an argument
+    flight = Flight.objects.get(pk=flight_id) # Get the flight whos ID = Flight ID ( pk refers to primary key, can also use id instead of pk)
+    return render(request, "flights/flight.html", {
+        "flight": flight # pass the flight class to flight.html
+    })
