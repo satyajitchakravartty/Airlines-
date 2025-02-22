75aa45bd00649b850e26927f650b69ff9fb09b19
Satyajit Chakravartty <shanuchakravartty@gmail.com>
2025-02-23 02:48:53 +0530

    Updated admin.py file for Passengers view to allow adding flights from admin panel by double clicking on the flight

--- flights/admin.py
+++ flights/admin.py
@@ -5,11 +5,15 @@ from .models import Flight, Airport, Passenger
 
 # Register your models here.
 
-# Update admin interface to display id, origin, destination, duration
+# Update admin interface of Flight to display id, origin, destination, duration
 class FlightAdmin(admin.ModelAdmin):
     list_display = ("id", "origin", "destination", "duration")
+
+# Update admin interface of Passenger
+class PassengerAdmin(admin.ModelAdmin):
+    filter_horizontal = ("flights", )
 
 # To use the admin app to be able to manipulate Airport, FLights and Passengers
 admin.site.register(Airport)
 admin.site.register(Flight, FlightAdmin) # Use the FlightAdmin UI to show id, origin, etc
-admin.site.register(Passenger)
+admin.site.register(Passenger, PassengerAdmin)
